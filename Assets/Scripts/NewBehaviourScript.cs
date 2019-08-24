4f7ab52860f2e6e0d98ca24538e45ff30258987f
tony <tony06010235@hotmail.com>
2019-08-24 14:38:19 +0800

    prototype

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+#if UNITY_EDITOR
+	new
+#endif
+	public Rigidbody2D rigidbody;
+	public float gravity = .5f;
+	public float speed = 2f;
+
+	void Start() {
+		m_radius = ((Vector2)transform.position - rigidbody.position).magnitude;
+	}
+
+    void FixedUpdate() {
+		float hori = Input.GetAxis("Horizontal");
+		Vector2 distance = (Vector2)transform.position - rigidbody.position;
+		rigidbody.position += (Vector2)Vector3.Cross(distance, Vector3.forward) * hori * speed * Time.fixedDeltaTime;
+		distance = rigidbody.position - (Vector2)transform.position;
+		float sqrRad = m_radius * m_radius;
+		rigidbody.position -= distance.normalized * m_radius * Time.deltaTime * m_jumpVelocity;
+		if (distance.sqrMagnitude != sqrRad) {
+			if (distance.sqrMagnitude > sqrRad) {
+				rigidbody.position = distance.normalized * m_radius;
+			} else {
+				rigidbody.position += distance.normalized * m_radius * Time.deltaTime * gravity;
+			}
+		}
+		rigidbody.transform.up = -distance;
+	}
+
+	float m_radius = 3f;
+	[SerializeField] float m_jumpVelocity = 0f;
+}
