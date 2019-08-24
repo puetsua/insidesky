874927644dd854e385abce6f8a01ae1eb28a2c65
White67 <practicer67@gmail.com>
2019-08-24 17:22:13 +0800

    地底人物像軸心轉向

--- Assets/Scripts/Player_2.cs
+++ Assets/Scripts/Player_2.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Player_2 : MonoBehaviour
 {
-
+    GameManager mgr { get { return GameManager.instance; } }
+    
+    public Rigidbody2D rigidbody;
+    public float gravity = .5f;
 	public float speed = 2f;
+    public float m_radius = 15f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+          //m_radius = mgr.worldRadius;
     }
 
     // Update is called once per frame
@@ -18,8 +22,25 @@ public class Player_2 : MonoBehaviour
     {
         float hori = Input.GetAxis("Horizontal");
         float vert = Input.GetAxis("Vertical");
+        Vector2 distance = (Vector2)transform.position;
 
         transform.Translate(new Vector2(hori, vert) * speed * Time.deltaTime);
+        rigidbody.transform.up = -distance;
+
+        float sqrRad = m_radius * m_radius;
+        //rigidbody.position -= distance.normalized * m_radius * Time.deltaTime;
+
+        if (distance.sqrMagnitude != sqrRad)
+        {
+            if (distance.sqrMagnitude > sqrRad)
+            {
+                rigidbody.position = distance.normalized * m_radius;
+            }
+            else
+            {
+                
+            }
+        }
 
         
     }
