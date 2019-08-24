7b2090b7501abdb1fd7918ed12643d9e0820abfa
Bo-Han Liao <hankofficer@gmail.com>
2019-08-24 15:38:40 +0800

    Added Gamemanager object.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,24 @@
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    public static GameManager instance = null;
 
+    void Awake()
+    {
+        // Singleton class
+        if(instance != null)
+        {
+            Destroy(instance);
+            return;
+        }
+        instance = this;
+    }
+    void Start()
+    {
+
+    }
+    void Update()
+    {   
+        
+    }
 }
