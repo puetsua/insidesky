5b3328ce08676c65d024f1ca0427a53c48d76bfa
White67 <practicer67@gmail.com>
2019-08-24 16:36:34 +0800

    地底移動

---
+++ Assets/Scripts/Player_2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_2 : MonoBehaviour
+{
+
+	public float speed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hori = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector2(hori, vert) * speed * Time.deltaTime);
+
+        
+    }
+}
